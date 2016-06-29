2dd3e367fe7f1471df293cc7515a3316847363e1
armengevorgyan <armeng@nil.am>
2016-06-29 14:29:48 +0400

    asdf

--- Unit1.pas
+++ Unit1.pas
@@ -8,6 +8,7 @@ uses
 
 type
   TForm1 = class(TForm)
+    procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
   public
@@ -20,5 +21,10 @@ var
 implementation
 
 {$R *.fmx}
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  ShowMessage('Hello');
+end;
 
 end.
